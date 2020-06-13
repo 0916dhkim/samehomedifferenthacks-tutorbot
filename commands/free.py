6a2ccc55f0ea7da3456a8b318a86b5516284a9b4
Donghyeon Kim <0916dhkim@gmail.com>
2020-06-13 17:19:45 +0900

    Implement Free Command

--- commands/free.py
+++ commands/free.py
@@ -1,6 +1,9 @@
 from ..discord_client import client
+from ..data import mentorAdapter
+from discord.ext.commands import Context
 
 
 @client.command()
-async def free(ctx):
-    pass
+async def free(ctx: Context):
+    mentorAdapter.set_busy(ctx.author, False)
+    ctx.message.channel.send("You are free!")
