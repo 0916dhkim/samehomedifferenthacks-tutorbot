d38a87d940a74d194fcbd2f02431db7cc8d1ddf3
UdAyAn123 <45636609+UdAyAn123@users.noreply.github.com>
2020-06-13 11:44:34 +0530

    Update register.py

--- commands/register.py
+++ commands/register.py
@@ -1,4 +1,38 @@
 from ..discord_client import client
+import math
+def create_mentor_card(db_data):
+    card_mentor = '''
+<strong>{} {}</strong> \n
+Primary language: <strong>{}</strong> 
+Additional languages: <strong>{}</strong> 
+Discord link: <a href="{}">{}</a> 
+    '''.format( \
+        db_data[0], \
+        db_data[1], \
+        db_data[2], \
+        db_data[3], \
+        db_data[4], \
+        db_data[4])
+    return card_mentor
+
+def question_card(quest_data):
+    question = '''
+Hello! Me {} name is. \n
+I apologize for distracting you.
+but if there is free time please help\n
+In general, the essence of the problem: {} \n
+Brief Description of the Question: {} \n
+Additional Information!
+The main language of development: {}
+    '''.format(quest_data['name'], quest_data['title'], quest_data['question'], quest_data['lang'])
+    return question
+
+def mentors_card(obj):
+    card = ''
+    for i in range(len(obj)):
+        card += '▫️ {} {} \n Primary language: {}\n Additional languages: {}\n Ссылка: {}\n\n'.format(
+            obj[i][0], obj[i][1], obj[i][2], obj[i][3], obj[i][4])
+    return card
 
 
 @client.command()
