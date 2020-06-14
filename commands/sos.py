071bf60e785c62dd85556b47048eab7cf053e774
Donghyeon Kim <0916dhkim@gmail.com>
2020-06-14 11:14:35 +0900

    Add Helper Functions For SOS Command

--- commands/sos.py
+++ commands/sos.py
@@ -1,6 +1,29 @@
 from discord_client import client
 from discord.ext.commands import Context
+from discord import Role, Member
 from data import mentorAdapter
+from typing import List
+
+
+# Return true if target member contains all required roles
+def hasAllRoles(member: Member, *roles: Role) -> bool:
+    required = set(roles)
+    for r in member.roles:
+        required.discard(r)
+    return len(required) == 0
+
+
+# Filter Members by required roles.
+async def filterMembersByRequirements(
+    members: List[Member], *roles: Role
+) -> List[Member]:
+    return list(
+        filter(
+            lambda m: hasAllRoles(m, *roles),
+            members
+        )
+    )
+
 
 # Command to notify mentors for help.
 @client.command()
