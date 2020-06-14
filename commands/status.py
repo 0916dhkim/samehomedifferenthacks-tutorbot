8b32975ea9b380ff85317689d4f55a7d5d114127
Donghyeon Kim <0916dhkim@gmail.com>
2020-06-14 11:31:20 +0900

    Implement Status Command

---
+++ commands/status.py
@@ -0,0 +1,13 @@
+from discord_client import client
+from data import mentorAdapter
+from discord.ext.commands import Context
+
+
+# Command to check the author's busy status.
+@client.command()
+async def status(ctx: Context):
+    is_busy = mentorAdapter.check_busy(ctx.author)
+    if is_busy:
+        await ctx.message.channel.send("You are busy!")
+    else:
+        await ctx.message.channel.send("You are free!")
