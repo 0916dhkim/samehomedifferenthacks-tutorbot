5f873602419de542cea45a51b2bc61ca14eb727d
dpbenton <37326801+dpbenton@users.noreply.github.com>
2020-06-13 20:43:37 -0500

    Update sos.py
    prints members and busy members to chat

--- commands/sos.py
+++ commands/sos.py
@@ -1,7 +1,22 @@
 from discord_client import client
-
+from discord.ext.commands import Context
+from data import mentorAdapter
 
 # Command to notify mentors for help.
 @client.command()
-async def sos(ctx):
-    pass
+async def sos(ctx: Context):
+
+    roles = ctx.author.guild.roles
+    await ctx.message.channel.send("roles: ")
+    for r in roles:
+        await ctx.message.channel.send(r)
+
+    members = ctx.author.guild.members
+    await ctx.message.channel.send("members: ")
+    for m in members:
+        await ctx.message.channel.send(m.name)
+
+    await ctx.message.channel.send("busy members: ")
+    busyMembers = mentorAdapter._busy_members
+    for b in busyMembers:
+        await ctx.message.channel.send(b)
