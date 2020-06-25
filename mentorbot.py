050ea78ecb813dd4cbc3b880b2b8ced658eb28f5
Donghyeon Kim <0916dhkim@gmail.com>
2020-06-25 12:09:24 +0900

    Send Greetings When Reconnected

--- mentorbot.py
+++ mentorbot.py
@@ -1,8 +1,10 @@
 # This module is for handling discord events.
 
 import logging
+import asyncio
 from config import config
 from discord_client import client
+from discord import TextChannel
 from commands import add_commands
 
 
@@ -13,9 +15,24 @@ def main():
     client.run(token)
 
 
+# Send greetings to all discord servers
+async def send_greetings():
+    tasks = []
+    message = "Tutorbot is updated and now online."
+    for guild in client.guilds:
+        if len(guild.text_channels) != 0:
+            # Find default text channel.
+            default_channel: TextChannel = guild.text_channels[0]
+            tasks.append(
+                default_channel.send(message)
+            )
+    await asyncio.wait(tasks)
+
+
 @client.event
 async def on_ready():
     print("Ready to start mentoring")
+    await send_greetings()
 
 
 @client.event
